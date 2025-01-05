dba6fcaee031904f91f2f15dacd9b9184bda5593
Yateen00 <codingdudelolhow@gmail.com>
2025-01-05 18:21:32 +0530

    removed presence:true constraints from User as not needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[[:^alnum:]]).+\z/, message: "must include at least one symbol, one digit, and one capital letter" }
-  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :gender, presence: true, inclusion: { in: %w[m f t n], message: "%{value} is not a valid gender" }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[[:^alnum:]]).+\z/, message: "must include at least one symbol, one digit, and one capital letter" }
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :gender, inclusion: { in: %w[m f t n], message: "%{value} is not a valid gender" }
 end
